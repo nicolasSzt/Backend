380b9bcee3cddb45900dff96df06ae63b905856c
Nicolas Steinjamer <nsteinjamer.ext@ticmas.com>
2025-06-03 16:43:19 -0300

    chore:test

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/ping", (request, response) => {
   response.send("<h1>Hola soy una respuesta de express</h1>");
 });
 
