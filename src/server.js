f5792fabc3631b9daed97d63162293d37e652d52
Nicolas Steinjamer <nicostein2134@gmail.com>
2025-07-04 13:46:36 -0300

    chore: resolve

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://front-sage-omega.vercel.app',
     methods: "GET,POST,PUT,DELETE",
   })
 );
